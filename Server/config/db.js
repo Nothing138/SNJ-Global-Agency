bed4c39b257f23c34a2a54cbf5c8064c4100f0e4
Mrinal Kanti Das <mkantidas138@gmail.com>
2026-04-17 04:43:35 +0600

    Connection in railway

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'game_routes',
+    host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
