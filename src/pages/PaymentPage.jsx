62a64360d5a7cc1307c926dbf1a15b33db856359
Mrinal Kanti Das <mkantidas138@gmail.com>
2026-04-26 01:01:39 +0600

    Stripe added

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -15,7 +15,7 @@ const PaymentPage = () => {
   useEffect(() => {
     const getSecret = async () => {
       try {
-        const res = await axios.post('http://localhost:5000/api/payment/create-intent', {
+        const res = await axios.post('https://snj-global-agency-backend.onrender.com/api/payment/create-intent', {
           amount: 50, 
           currency: 'usd',
           customerName: 'Juboraj'
