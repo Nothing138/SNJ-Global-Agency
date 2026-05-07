904d3a27cc390bc9c308be98de87a3757965e887
Mrinal Kanti Das <mkantidas138@gmail.com>
2026-05-08 01:07:08 +0600

    production API URL update

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,8 @@ const io = new Server(server, {
 app.use(cors({
     origin: [
         'https://snj-global-agency-3el5.vercel.app',
-        'https://snj-global-agency-backend.onrender.com',
+        'https://snj-global-agency-backend-nhxq.onrender.com',
+        //'https://snj-global-agency-backend.onrender.com',
         'http://localhost:5173',
         'http://localhost:5174'
     ],
